4eac261db8fd4729d2800ddec07df7ddd8a98ad4
Meraj Vindira <61262712+Meraj6091@users.noreply.github.com>
2021-05-03 15:20:14 +0530

    Add files via upload

---
+++ src/com/algo/cw/Main.java
@@ -0,0 +1,11 @@
+package com.algo.cw;
+import java.lang.*;
+
+    public class Main {
+        public static void main(String[] args) {
+
+            MaxFlow maxFlow=new MaxFlow();
+            maxFlow.display();
+        }
+
+    }
